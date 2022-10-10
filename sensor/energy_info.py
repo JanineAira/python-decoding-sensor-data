c0cccc2f9ec8ee6f1be420f93b3e5bafb42706ca
JanineDG <46197146+JanineAira@users.noreply.github.com>
2022-10-10 22:49:59 +0800

    Completed module

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -2,18 +2,18 @@ from house_info import HouseInfo
 from datetime import date
 
 class EnergyData(HouseInfo):
-    ENERGY_PER_BULB = 0.2
+    ENERGY_PER_BULB = 0.2 
     ENERGY_BITS = 0x0F0
 
     def _get_energy(self, rec):
-        energy = int(rec, base-16)
+        energy = int(rec, base=16)
         energy = energy & self.ENERGY_BITS
         energy = energy >> 4
         return energy
 
     def _convert_data(self, data):
         recs = []
-
+        
         for rec in data:
             recs.append(self._get_energy(rec))
         return recs
