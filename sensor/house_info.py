ae009040b4d8ca33c4931c48b28d45957329ef85
JanineDG <46197146+JanineAira@users.noreply.github.com>
2022-10-10 19:51:40 +0800

    Completed module

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -2,8 +2,7 @@ class HouseInfo(object):
     def __init__(self, data):
         self.data = data
 
-    def get_data_by_area(self, field, rec_area):
-        rec_area = 0
+    def get_data_by_area(self, field, rec_area = 0):
         field_data = []
 
         for record in self.data:
